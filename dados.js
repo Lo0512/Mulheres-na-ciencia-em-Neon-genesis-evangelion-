3e0b04586270e6baf220ada1050931052134db42
Lo0512 <heloisadealmeidabatista.13@gmail.com>
2024-09-07 22:33:59 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,90 @@
+let dado = [
+    {
+        titulo: "Rei Ayanami ",
+        profissao: "Piloto do Evangelion Unidade 0, a enigmática personagem de Neon Genesis Evangelion",
+        introducao: "Apresenta um caso particular quando se trata de sua influência no movimento de mulheres na ciência. Diferentemente das outras personagens, Rei possui uma identidade mais ambígua e está mais ligada à tecnologia e à ciência de uma forma passiva. No caso, ela atua como um protótipo humano, sempre obedecendo sem contestamentos. No entanto, essa representação não a torna menos influente.",
+
+        desenvolvimento: "Rei simboliza conceitos complexos como criação artificial, solidão e a busca por identidade. Essa ligação com a ciência e a tecnologia, mesmo que de forma passiva, pode inspirar meninas a se interessarem por essas áreas, especialmente aquelas que se veem como mais introvertidas ou introspectivas. A natureza enigmática de Rei pode despertar a curiosidade das meninas sobre o mundo científico, incentivando-as a buscar respostas e a explorar novas áreas do conhecimento.A inclusão de personagens como Rei, com características e histórias únicas, contribui para uma representação mais diversa de mulheres na ciência, mostrando que não existe um único modelo de cientista. No entanto, é importante ressaltar que a representação de Rei é complexa. A natureza artificial da personagem pode levar a interpretações ambíguas sobre seu papel como modelo para jovens mulheres. Além disso, a idealização de Rei como um ser quase perfeito pode criar expectativas irrealistas sobre o que significa ser uma cientista.",
+        resumo: "Rei Ayanami, apesar de sua natureza complexa, pode inspirar meninas a se interessarem pela ciência, especialmente aquelas que se identificam com sua introspecção e sua conexão com a tecnologia. No entanto, é fundamental que essa representação seja analisada de forma crítica, considerando tanto os aspectos positivos quanto os desafios que ela apresenta.",
+        link: "http://it.wikipedia.org/wiki/Rei_Ayanami"
+    },
+    {
+        titulo: "Asuka Langley Soryu",
+        profissao: "Piloto do Evangelion Unidade-02",
+        introducao: "Asuka Langley Soryu, a piloto do Evangelion Unit-02, é outra personagem marcante de Neon Genesis Evangelion que exerce grande influência, especialmente sobre o público feminino. Apesar de sua personalidade complexa e muitas vezes difícil, Asuka carrega em si qualidades que a tornam um modelo inspirador para jovens garotas.",
+        desenvolvimento: "Sua representação como uma prodigiosa piloto, com habilidades excepcionais e uma determinação inabalável, incentiva jovens a acreditarem em seu potencial e a perseguirem seus objetivos. Asuka demonstra uma forte vontade de ser independente e autossuficiente. Quebrando estereótipos de gênero, Asuka mostra que meninas podem ser tanto vulneráveis quanto poderosas, e que não há um molde único para o sucesso. Sua jornada, marcada por desafios e traumas, mas também pela superação, inspira jovens a enfrentarem suas próprias dificuldades e a crescerem a partir delas. Asuka é um exemplo de que a busca pela perfeição e o desejo de ser a melhor podem ser motivadores para o crescimento pessoal (entretanto, a perfeição não existe, como demonstrado em toda a trama). Sua autoconfiança, mesmo que às vezes seja excessiva, inspira meninas a acreditarem em si mesmas e em suas capacidades. Em diversas ações, é possível observar sua coragem diante dos desafios! Além disso, ao mostrar que é normal ser imperfeito, ela ajuda as meninas a se identificarem com ela e a perceberem que é possível ser forte e vulnerável ao mesmo tempo.",
+        resumo: "Em resumo, Asuka Langley Soryu é uma personagem que, apesar de suas imperfeições, representa a força, a determinação e a busca pela excelência. Sua história inspira jovens a acreditarem em si mesmas e a perseguirem seus sonhos, mesmo diante dos desafios. Assim como Misato, Asuka se torna um símbolo de empoderamento feminino, inspirando meninas a serem autênticas e a seguirem seus próprios caminhos.",
+        link: "https://evangelion.fandom.com/wiki/Asuka_Shikinami_Langley#:~:text=Asuka_Shikinami_Langley%20(%E5%BC%8F%E6%B3%A2%E3%83%A2%E3%82%B9%E3%82%B3%83%A9%E3%83%B3%E3%82%B0%E3%83%AC%E3%83%BC?)"
+    },
+
+    {
+        titulo: "Ikari",
+        profissao: "Cientista e Co-criadora dos Evangelions",
+        introducao: "Yui Ikari, a mãe de Shinji Ikari, foi uma cientista brilhante que desempenhou um papel crucial na criação dos Evangelions. Sua presença, embora limitada na série, é sentida em todo o enredo.",
+        desenvolvimento: "Yui era uma pioneira em sua área, com ideias inovadoras e uma visão futurista. Seu trabalho na criação da Unidade-01 e sua subsequente sincronização com a máquina são testemunhos de sua genialidade. A personagem representa a figura do cientista visionário, capaz de conceber e realizar projetos ambiciosos. Sua história, marcada por mistério e sacrifício, a torna uma figura icônica na série.",
+        resumo: "Yui Ikari é a mente por trás do Projeto Eva, uma cientista visionária que deixou um legado duradouro. Suahistória inspira aqueles que buscam inovar e desafiar os limites da ciência",
+        link: "https://evangelion.fandom.com/wiki/Yui_Ikari"
+    },
+
+    {
+        titulo: "Naoko Akagi",
+        profissao: "Cientista da NERV",
+        introducao: "Naoko Akagi era uma jovem cientista com um futuro promissor. Sua inteligência e curiosidade a tornavam uma grande aliada de Ritsuko no Projeto Eva. Naoko demonstrava um grande potencial científico e uma paixão pela pesquisa.", 
+        descricao:"Sua morte prematura foi uma grande perda para a NERV, e sua ausência é sentida ao longo da série. A personagem representa a figura do jovem cientista cheio de esperança e entusiasmo, que busca contribuir para o avanço da ciência. Sua história serve como um lembrete da importância de investir em educação e pesquisa.",
+        resumo: "Naoko Akagi era uma jovem promessa da ciência, cuja morte trágica deixou uma marca profunda na série. Sua história inspira jovens cientistas a seguirem seus sonhos e a fazerem a diferença.",
+        link: "https://evangelion.fandom.com/wiki/Naoko_Akagi"
+    },
+    {
+        titulo: "Misato Katsuragi",
+        profissao: "Cientista e militar",
+        introducao: "A icônica personagem de Neon Genesis Evangelion vai além de ser apenas uma piloto. Ela é uma cientista brilhante, líder nata e figura materna, desafiando estereótipos de gênero e inspirando gerações. Sua força, determinação e inteligência a tornam um modelo inspirador para jovens mulheres que sonham com carreiras na ciência.",
+        desenvolvimento: "Misato quebra barreiras ao mostrar que mulheres podem ser tanto poderosas quanto inteligentes em áreas tradicionalmente dominadas por homens. Ela é uma prova viva de que é possível conciliar uma carreira exigente com a vida pessoal. Sua complexidade e humanidade a tornam uma personagem com a qual o público feminino pode se identificar. Mas o impacto de Misato vai além da ficção. Ao ser representada como uma cientista de sucesso, ela incentiva meninas a explorarem suas paixões e a buscarem conhecimento. Sua resiliência diante dos desafios demonstra que é possível superar obstáculos e alcançar seus objetivos.",
+        resumo: "Em resumo, Misato Katsuragi não é apenas uma personagem de anime, mas um símbolo de empoderamento feminino. Ela inspira meninas a sonhar grande e a acreditar em suas capacidades, contribuindo para a criação de um futuro mais justo e igualitário para todas as mulheres.",
+        link: "https://en.wikipedia.org/wiki/Misato_Katsuragi#:~:text=Misato_Katsuragi%20(Japanese:%E8%91%9B%E5%9F%8E%E3%83%9F%E3%82%B5%E3%83%88",
+    },
+    {
+        titulo: "Ritsuko Akagi",
+        profissao: "Cientista-chefe da NERV",
+        introducao: "Ritsuko Akagi é a cientista-chefe da NERV, uma figura central no Projeto Evangelion. Sua mente analítica e conhecimento profundo em diversas áreas da ciência a tornam uma das personagens mais inteligentes da série.",
+        desenvolvimento: "Ritsuko demonstra uma paixão incansável pela pesquisa e uma habilidade excepcional para solucionar problemas complexos. Seu trabalho na NERV é fundamental para o desenvolvimento de tecnologias avançadas e para a compreensão dos Anjos. A personagem representa a figura do cientista dedicado, que busca incessantemente por respostas e novas descobertas. Sua mente lógica e racional contrasta com as emoções turbulentas que a assombram, criando uma personagem complexa e fascinante.",
+        resumo: "Ritsuko Akagi é mais do que apenas uma cientista; ela é a encarnação da curiosidade intelectual e da busca pelo conhecimento. Sua contribuição para o Projeto Eva é inegável, e sua história inspira aqueles que buscam uma carreira nas ciências.",
+        link: "https://evangelion.fandom.com/wiki/Ritsuko_Akagi"
+    },
+
+    {
+        titulo: "Hikari Horaki",
+        profissao: "Representante de classe",
+        introducao: "Hikari Horaki é a representante de classe na escola dos pilotas das unidades O, 1 e 2.Sua presença traz um toque de normalidade e humanidade ao ambiente escolar dos protagonistas.",
+        desenvolvimento: "Hikari demonstra liderança e responsabilidade em seu papel de representante de classe. Ela é uma aluna dedicada e uma amiga leal, sempre pronta para ajudar seus colegas. Sua capacidade de equilibrar suas responsabilidades escolares com sua vida pessoal a torna um exemplo positivo para os jovens.",
+        resumo: "Hikari Horaki representa a figura do estudante dedicado e responsável. Sua história inspira jovens a serem líderes em suas comunidades escolares e a valorizarem a amizade e a responsabilidade.",
+        link: "Hikari Horaki](https://evangelion.fandom.com/wiki/Hikari_Horaki)"
+    },
+
+    {
+        titulo: "Maya Ibuki",
+        profissao: "Operadora da NERV",
+        introducao: "Maya Ibuki trabalha como operadora na NERV e é assistente de Ritsuko Akagi. Ela é uma personagem dedicada e competente, que contribui significativamente para as operações da NERV.",
+        desenvolvimento: "Maya demonstra uma grande habilidade técnica e um profundo conhecimento em sistemas de computação. Sua dedicação ao trabalho e sua lealdade à equipe da NERV são evidentes em suas ações. Maya representa a figura do profissional técnico que trabalha nos bastidores para garantir o sucesso das operações.",
+        resumo: "Maya Ibuki é mais do que apenas uma operadora; ela é a personificação da competência técnica e da dedicação. Sua história inspira aqueles que buscam carreiras em áreas técnicas e de suporte.",
+        link: "Maya Ibuki](https://evangelion.fandom.com/wiki/Maya_Ibuki)"
+    },
+
+    {
+        titulo: "Kyoko Zeppelin Sohryu",
+        profissao: "Cientista da filial alemã do Projeto Evangelion",
+        introducao: "Kyoko Zeppelin Sohryu, mãe de Asuka Langley Soryu, era uma cientista da filial alemã do Projeto Evangelion. Sua história é trágica e tem um grande impacto na vida de Asuka.",
+        desenvolvimento: "Kyoko era uma cientista brilhante, dedicada ao avanço do Projeto Evangelion. Sua paixão pela pesquisa e sua determinação em contribuir para o projeto são evidentes. No entanto, sua história também é marcada por tragédia, o que adiciona uma camada de complexidade à sua personagem.",
+        resumo: "Kyoko Zeppelin Sohryu representa a figura do cientista dedicado, cuja paixão pela pesquisa é inegável. Sua história serve como um lembrete dos sacrifícios que muitas vezes acompanham a busca pelo conhecimento.",
+        link: "Kyoko Zeppelin Sohryu](https://evangelion.fandom.com/wiki/Kyoko_Zeppelin_Soryu)"
+    },
+
+    {
+        titulo: "Mari Makinami Illustrious",
+        profissao: "Piloto de Eva",
+        introducao: "Mari Makinami Illustrious, introduzida nos filmes Rebuild of Evangelion, é uma piloto de Eva com uma personalidade única e enérgica, que adiciona uma nova dinâmica à série.",
+        desenvolvimento: "Mari demonstra uma habilidade excepcional como piloto e uma abordagem destemida aos desafios. Sua energia e entusiasmo são contagiantes, e ela traz uma nova perspectiva ao grupo de pilotos. Mari representa a figura do jovem inovador, sempre pronto para enfrentar novos desafios com coragem e determinação.",
+        resumo: "Mari Makinami Illustrious é mais do que apenas uma piloto; ela é a personificação da inovação e da coragem. Sua história inspira jovens a enfrentarem desafios com entusiasmo e a explorarem novas possibilidades.",
+        link: "Mari Makinami Illustrious](https://evangelion.fandom.com/wiki/Mari_Makinami_Illustrious)"
+    }
+
+]
